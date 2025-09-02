13ffc557ad89956a62b37e28f1fdebc99f1bba5c
syedroshanriyan-pc <syedroshanriyan.pc@gmail.com>
2025-09-02 20:59:03 +0530

    Update app.py

--- durood_counter/app.py
+++ durood_counter/app.py
@@ -19,7 +19,7 @@ users = db["users"]
 recitations = db["recitations"]
 
 TARGET = 1000000
-EVENT_DATE = datetime(2025, 9, 5, 5, 30, 0)
+EVENT_DATE = datetime(2025, 9, 5, 12, 30, 0)
 
 
 @app.route("/")
@@ -119,4 +119,5 @@ def logout():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
